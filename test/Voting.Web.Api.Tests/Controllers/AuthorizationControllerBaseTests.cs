cd1fa545a0f75287adc0bfcb1085b8dbc1b49e1d
Nick Butcher <butchern@mail.gvsu.edu>
2016-11-21 17:02:55 -0500

    Add stubbed test projects

---
+++ test/Voting.Web.Api.Tests/Controllers/AuthorizationControllerBaseTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Voting.Web.Api.Tests.Controllers
+{
+    using System.Threading.Tasks;
+    using Api.Controllers;
+    using Library.Session;
+    using Library.User;
+    using Xunit;
+
+    public class AuthorizationControllerBaseTests
+    {
+        [Fact]
+        public async Task GetRequestUser_ReturnsUser()
+        {
+            // Arrange
+            var target = new AuthorizationControllerBaseStub(null, null);
+
+            // Act
+            var result = await target.StubMethod();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        private class AuthorizationControllerBaseStub : AuthorizationControllerBase
+        {
+            public AuthorizationControllerBaseStub(ISessionRepository sessionRepository, IUserRepository userRepository)
+                : base(sessionRepository, userRepository)
+            {
+
+            }
+
+            public Task<IUser> StubMethod()
+            {
+                return this.GetRequestUser();
+            }
+        }
+    }
+}
