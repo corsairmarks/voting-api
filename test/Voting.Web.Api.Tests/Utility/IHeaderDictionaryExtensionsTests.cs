0af8ffbfbe6e22adfbedc2fbccf32ad04d2145d2
Nick Butcher <butchern@mail.gvsu.edu>
2016-11-28 20:09:28 -0500

    Add xunit theory (output iffy because it can't serialize/deserialize Guid by default)

--- test/Voting.Web.Api.Tests/Utility/IHeaderDictionaryExtensionsTests.cs
+++ test/Voting.Web.Api.Tests/Utility/IHeaderDictionaryExtensionsTests.cs
@@ -1,7 +1,6 @@
 ﻿namespace Voting.Web.Api.Tests.Utility
 {
     using System;
-    using System.Collections.Generic;
     using Api.Utility;
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Primitives;
@@ -9,7 +8,16 @@
 
     public class IHeaderDictionaryExtensionsTests
     {
-        public static IEnumerable<> SessionTokens; // TODO: theory data
+        private static readonly Guid RealGuid = Guid.Parse("c3f21140-064d-41d1-bc2b-ab0761a16a1f");
+
+        public static TheoryData<string, Guid?> SessionTokens { get; } = new TheoryData<string, Guid?>
+        {
+            { null, null },
+            { "", null },
+            { "FOO", null },
+            { "guid", null },
+            { RealGuid.ToString(), RealGuid },
+        };
 
         [Fact]
         public void GetSessionToken_NullHeaders_Throws()
@@ -27,7 +35,8 @@
         }
 
         [Theory]
-        [MemberData("SessionTokens")]
+        [MemberData(nameof(SessionTokens))]
+        // NOTE: Because Guid is not serializable by default, Xunit can't output one line per test - I will be opening an issue on github
         public void GetSessionHeader_Values(string header, Guid? expected)
         {
             // Arrange
